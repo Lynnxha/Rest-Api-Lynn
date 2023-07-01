25d20c9918ee25fa08036417819060764d69ed57
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2023-07-01 20:26:40 +0700

    Update DB

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Lynndev'// yourname
+global.MONGO_DB_URI = "mongodb+srv://kibieptrf81:kibietika12@lynnrestapikey.zayd9ai.mongodb.net/" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "kibieptrf81@gmail.com" //email
+global.email_password = "gwbjshmxidpjwfgp" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
